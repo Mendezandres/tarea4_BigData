df10d3980da10fa55eb09c628478eadadc9ea76b
Mendezandres <122476598+Mendezandres@users.noreply.github.com>
2025-11-21 20:32:07 -0500

    Add CRUD operations for products and clients

--- scripts/consultas_basicas.js
+++ scripts/consultas_basicas.js
@@ -1,21 +1,56 @@
-// Inserción de documentos
-db.usuarios.insertOne({
-  nombre: "Carlos Ruiz",
-  correo: "carlos@gmail.com",
-  direccion: "Cra 10 #45-23",
-  telefono: "3119876543",
-  fecha_registro: "2024-05-01"
-})
+1.	Inserción de documentos
+//Insertar varios productos:
+db.productos.insertMany([
+  {
+    _id: ObjectId(),
+    nombre: "Mouse Gamer Logitech",
+    categoria_id: ObjectId(""),
+    precio: 160000,
+    stock: 15,
+    descripcion: "Mouse RGB 8000 DPI",
+    fecha_creacion: "19/11/2025",
+    estado: "activo"
+  },
+  {
+    _id: ObjectId(),
+    nombre: "Teclado Mecánico HyperX",
+    categoria_id: ObjectId(""),
+    precio: 360000,
+    stock: 8,
+    descripcion: "Mecánico switches Cherry Red",
+    fecha_creacion:"19/11/2025",
+    estado: "activo"
+  }
+])
 
-// Seleccion de un documento
-db.productos.find({ categoria: "Electrónica" })
-//Muestra todos los productos pertenecientes a la categoría "Electrónica"
 
-// Actualizar stock de un producto
+
+2.	Selección de documentos
+//Consultar todos los clientes: 
+db.clientes.find()
+//Consultar solo algunos campos: 
+db.clientes.find({}, { nombre: 1, email: 1 })
+//Consultar productos activos: 
+db.productos.find({ estado: "activo" })
+
+
+3.	Actualización de documentos 
+//cambiar el nivel de un cliente: 
+db.clientes.updateOne(
+  { email: "wtrehearn1@ftc.gov },
+  { $set: { nivel_cliente: "gold" } }
+)
+//Descontar stock de un producto
 db.productos.updateOne(
-  { nombre: "G Pro" },
-  { $set: { stock: 30 } }
+  { nombre: "Mouse Gamer Logitech" },
+  { $inc: { stock: -1 } }
 )
 
-// Eliminar un usuario inactivo
-db.usuarios.deleteOne({ activo: false })
+
+
+4.	Eliminación de documentos 
+//Borrar un cliente: 
+db.clientes.deleteOne({ email: "pkensitt5@ucla.edu" })
+//Borrar productos sin stock: 
+db.productos.deleteMany({ stock: { $lte: 0 } })
+
