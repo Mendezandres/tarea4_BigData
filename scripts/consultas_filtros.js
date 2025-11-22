e03f3c1719b5ed9c7aee481712ec563fd189eb8e
Mendezandres <122476598+Mendezandres@users.noreply.github.com>
2025-11-21 20:27:12 -0500

    Add various MongoDB queries and operators examples

--- scripts/consultas_filtros.js
+++ scripts/consultas_filtros.js
@@ -1,9 +1,47 @@
-// Usuarios registrados después de cierta fecha
-db.usuarios.find({ fecha_registro: { $gt: "2024-01-01" } })
+//1. Igualdad : 
+db.clientes.find({ estado: "activo" })
 
-//Productos con precio entre 100,000 y 500,000
-db.productos.find({ precio: { $gte: 100000, $lte: 500000 } })
+//2. mayor que: 
+db.productos.find({ precio: { $gt: 300000 } })
 
-//Pedidos con cantidad mayor a 3
-db.pedidos.find({ cantidad: { $gt: 3 } })
-// Identifica los pedidos grandes o con múltiples unidades.
+//3. menor o igual que: 
+db.productos.find({ stock: { $lte: 100 } })
+
+
+//4. Operador $and, buscar productos caros y con poco stock:
+db.productos.find({
+  $and: [
+    { precio: { $gt: 200000 } },
+    { stock: { $lt: 90 } }
+  ]
+})
+
+
+
+//5. Operador $or, buscar clientes gold o platinum
+db.clientes.find({
+  $or: [
+    { nivel_cliente: "gold" },
+    { nivel_cliente: "platinum" }
+  ]
+})
+
+
+
+//6. Operador $in, buscar productos de varias categorías
+db.productos.find({
+  categoria_id: { $in: [
+    ObjectId("691e20c9fc13ae337f541061"),
+    ObjectId("691e20c9fc13ae337f541067")
+  ]}
+})
+
+
+
+//7. búsqueda textual (se crea el índice)
+db.productos.createIndex({ nombre: "text" })
+db.productos.find({
+  $text: { $search: "gamer" }
+})
+
+
